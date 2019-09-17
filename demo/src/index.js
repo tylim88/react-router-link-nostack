c631c77d4559fb3e9f6e164ef42b8d271a747ed2
tylim <limteikyee@gmail.com>
2019-09-18 02:29:40 +0800

    update

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,21 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
-
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import Example from '../../src'
 
+const history = createBrowserHistory()
 class Demo extends Component {
-  render() {
-    return <div>
-      <h1>react-router-link-nostack Demo</h1>
-      <Example/>
-    </div>
-  }
+	render() {
+		return (
+			<Router history={history}>
+				<div>
+					<h1>react-router-link-nostack Demo</h1>
+					<Example to={{ a: 1 }}>test</Example>
+				</div>
+			</Router>
+		)
+	}
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
