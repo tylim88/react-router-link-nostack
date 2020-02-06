e6776610bd9ec53e96caf0f206ae6dce6f8b9139
tylim <limteikyee@gmail.com>
2020-02-06 22:32:18 +0800

    1.change nwb to neutrino 2. use peer dependency instead of dependency

---
+++ src/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render } from 'react-dom'
+import LinkNoStack from './components/LinkNoStack'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const Demo = () => {
+	return (
+		<BrowserRouter basename={'/react-router-link-nostack'}>
+			<div>
+				<h1>react-router-link-nostack Demo</h1>
+				<LinkNoStack to='/'>to index</LinkNoStack>
+				<br />
+				<LinkNoStack to='/123'>to 123</LinkNoStack>
+			</div>
+			<br />
+			<Switch>
+				<Route
+					exact
+					path='/'
+					render={() => {
+						return <p> Index{console.log('rerender index')}</p>
+					}}
+				/>
+				<Route
+					exact
+					path='/123'
+					render={() => {
+						return <p> 123{console.log('rerender 123')}</p>
+					}}
+				/>
+			</Switch>
+		</BrowserRouter>
+	)
+}
+
+render(<Demo />, document.getElementById('root'))
